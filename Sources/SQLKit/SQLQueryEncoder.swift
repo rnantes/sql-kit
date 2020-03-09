0d26461573345eff3d30f9ef9471b4c33a48cfb1
{rnantes} <{reid.nantes@gmail.com}>
2020-03-08 20:41:40 -0400

    removed non encodable encode if present

--- Sources/SQLKit/SQLQueryEncoder.swift
+++ Sources/SQLKit/SQLQueryEncoder.swift
@@ -109,20 +109,7 @@ private final class _Encoder: Encoder {
             }
         }
 
-        mutating func encodeIfPresent<T>(_ value: T?, forKey key: Key) throws {
-            print("encodeIfPresent T -----")
-            print(key)
-            print(value)
-            if let value = value {
-                print("do nothing")
-                return
-            } else {
-                print("keep nil")
-                self.encoder.row.append((self.column(for: key), SQLLiteral.null))
-            }
-        }
-
-        mutating func encodeIfPresent<T>(_ value: T?, forKey key: Key) throws where T : Encodable {
+        mutating func encodeIfPresent<T>(_ value: T?, forKey key: Self.Key) throws where T : Encodable {
             print("encodeIfPresent T Encodable -----")
             print(key)
             print(value)
