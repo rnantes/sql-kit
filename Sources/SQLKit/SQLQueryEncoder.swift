e2f47a17a43d75856bf3d55d54a4e9566824f7dd
{rnantes} <{reid.nantes@gmail.com}>
2020-03-08 20:52:33 -0400

    add all encode if present types

--- Sources/SQLKit/SQLQueryEncoder.swift
+++ Sources/SQLKit/SQLQueryEncoder.swift
@@ -147,6 +147,73 @@ private final class _Encoder: Encoder {
     }
 }
 
+private extension _Encoder._KeyedEncoder {
+    mutating func encodeIfPresentBase<T>(_ value: T?, forKey key: Self.Key) throws where T : Encodable {
+        if let value = value {
+            try encode(value, forKey: key)
+        } else {
+            print("keep nil")
+            self.encoder.row.append((self.column(for: key), SQLLiteral.null))
+        }
+    }
+
+    mutating func encodeIfPresent(_ value: Bool?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: String?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Double?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Float?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Int?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Int8?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Int16?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Int32?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: Int64?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: UInt?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: UInt8?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: UInt16?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: UInt32?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+
+    mutating func encodeIfPresent(_ value: UInt64?, forKey key: Key) throws {
+        try encodeIfPresentBase(value, forKey: key)
+    }
+}
+
 private extension _Encoder {
     /// This is a custom implementation which does not require Foundation as opposed to the one at which needs CharacterSet from Foundation https://github.com/apple/swift/blob/master/stdlib/public/Darwin/Foundation/JSONEncoder.swift
     ///
